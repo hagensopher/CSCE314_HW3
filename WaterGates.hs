abde76af26e19f0b60f2ffc004c581e0f531ad38
hagensopher <hagensopher13@gmail.com>
2021-10-18 17:07:35 -0500

    rework of q2

--- WaterGates.hs
+++ WaterGates.hs
@@ -6,15 +6,22 @@
 --final day change the last gate
 numofGatesOpen = 0
 waterGate :: Int -> Int 
-waterGate 0 = numofGatesOpen
-waterGate 1 = 1
+waterGate 0 = 1
+{--waterGate 1 = 1
 waterGate 2 = 2
 waterGate 3 = 3
-waterGate n   --this is the one where the ith and nth case should happen
-{- waterGate n 
-    | n == (n-(n-1)) = let numofGatesOpen = numofGatesOpen in numofGatesOpen + n + waterGate(n-1)
+--}
+-- waterGate n   --this is the one where the ith and nth case should happen
+{--
+waterGate n 
+    | n == (n-(n-1)) = let numofGatesOpen = numofGatesOpen in numofGatesOpen + n + waterGate(n-1) 
     | n == (n-(n-2)) = let numofGatesOpen =  numofGatesOpen in numofGatesOpen - n `div` 2 + waterGate(n-1)
     | n == (n-(n-3)) = let numofGatesOpen =  numofGatesOpen in numofGatesOpen - n `div` 3 + waterGate(n-1)
-    | n == 1 = let numofGatesOpen = numofGatesOpen in numofGatesOpen - 1 + waterGate(n-1)
+    -- | n == 1 = let numofGatesOpen = numofGatesOpen in numofGatesOpen - 1 + waterGate(n-1)
     | otherwise = let numofGatesOpen = numofGatesOpen in waterGate(n-1)
-    -}
+--}
+waterGate n 
+    | n == n-(n-1) = 4 + waterGate(n-1)
+    | n == n-(n-2) = mod (n+1) 2 - waterGate(n-1)
+    | n == n-(n-3) = mod (n+2) 3 + waterGate(n-1)
+    | otherwise = 1 + waterGate(n-1)
