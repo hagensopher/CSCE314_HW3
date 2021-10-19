0827ce090fad08206b46021d37b1804708caf34f
hagensopher <hagensopher13@gmail.com>
2021-10-19 15:38:11 -0500

    iterating through the list works

--- flip12.hs
+++ flip12.hs
@@ -1,11 +1,11 @@
 --this is the outerloop which goes through the days
 --i will count up
-looper:: [Bool] -> Integer -> Integer -> [Bool]
-looper myList 0 _ = myList
-looper myList n i= looper (flip12 myList i) (n-1) (i+1)
+looper:: [Bool] -> Integer ->Integer ->Integer -> [Bool]
+looper myList _ _ 0= myList
+looper myList n i stopper= looper (flip12 myList 1 i) n (i+1) (stopper -1)
 
 
 --this is the inner loop which checks each time 
-flip12 :: [Bool] -> Integer -> [Bool]
-flip12 [] _ = []
-flip12 (x:xs) i = if(i==2) then (not x):flip12 xs (i+1) else x:flip12 xs (i+1)
+flip12 :: [Bool] -> Integer-> Integer -> [Bool]
+flip12 [] _ _ = []
+flip12 (x:xs) n i = if(mod n i == 0) then (not x):flip12 xs (n+1) i else x:flip12 xs (n+1) i
