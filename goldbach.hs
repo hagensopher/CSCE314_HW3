6d05d9acde664f3a795f7de293756bcf30f07169
hagensopher <hagensopher13@gmail.com>
2021-10-20 16:39:00 -0500

    looping works?

--- goldbach.hs
+++ goldbach.hs
@@ -6,7 +6,17 @@ isDoubleSquare (x:xs) n
     | n == x = True
     | x > n = False
 
+--this function takes a list of all primes and composite number it is checking n
 loopOfPrimes :: [Integer] -> Integer -> Bool
-loopOfPrimes (x:xs) n 
-    | x < n =
-    | even (x-n) = isDoubleSquare [2*(x^2)]
+loopOfPrimes (x:xs) n --(x:xs) is the list of primes , n is the composite number
+    | x > n = False
+    | even (n-x) = if isDoubleSquare ([2*(x^2) | x <- [1..]]) (n-x) then True else loopOfPrimes xs n
+    | odd (n-x) = loopOfPrimes xs n
+
+--isPrime Functions from class
+isPrime:: Integer -> Bool
+isPrime 1 = False
+isPrime n = ([] == [x | x <- [2..(n-1)], n `mod` x == 0])
+
+listOfPrimes:: [Integer]
+listOfPrimes  = [k | k<-[1..], isPrime k]
