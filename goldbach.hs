a4ae917aa772c79e30f6e4125bd51eb11cc1ff6b
hagensopher <hagensopher13@gmail.com>
2021-10-20 12:58:11 -0500

    question 3 starting

--- goldbach.hs
+++ goldbach.hs
@@ -1,10 +1,12 @@
+--this function checks if a number is in a list of a 2 * perfect square
+isDoubleSquare:: [Integer] -> Integer-> Bool
 
+isDoubleSquare (x:xs) n
+    | x < n = isDoubleSquare xs n
+    | n == x = True
+    | x > n = False
 
-oddNums:: [Integer]
-oddNums = [3,5 .. ]
-
-compositeOdds :: [Integer]
-compositeOdds = [n | n <- oddNums, not --prime]
-
-goldbachNum :: Int
-goldbachNum = if prime * (2* something^something) is in list of compositeOdds then copmositeOdds else goldbachNum(next prime?)
+loopOfPrimes :: [Integer] -> Integer -> Bool
+loopOfPrimes (x:xs) n 
+    | x < n =
+    | even (x-n) = isDoubleSquare [2*(x^2)]
